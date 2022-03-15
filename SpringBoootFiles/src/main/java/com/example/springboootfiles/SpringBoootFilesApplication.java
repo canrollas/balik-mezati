e44182986e93268272232bb788fc43b82e2c52bb
canrollas <canrollas@gmail.com>
2022-03-16 01:57:20 +0300

    Spring Boot Appended -4

--- SpringBoootFiles/src/main/java/com/example/springboootfiles/SpringBoootFilesApplication.java
+++ SpringBoootFiles/src/main/java/com/example/springboootfiles/SpringBoootFilesApplication.java
@@ -2,12 +2,23 @@ package com.example.springboootfiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBoootFilesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoootFilesApplication.class, args);
     }
+    @GetMapping(value = "/")
+    public String bringSomeCoffe() {
+        return "Hello, can i get some coffe?";
+    }
 
+    @GetMapping(value = "/ankara")
+    public String sayHiToVisiters() {
+        return "Hello, World!";
+    }
 }
