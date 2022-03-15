b24277f9037700347056a17015ffcebea70783de
canrollas <canrollas@gmail.com>
2022-03-16 00:06:02 +0300

    General Changes  in spring boot

---
+++ fishAuction/src/main/java/com/example/fishAuction/FishAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.fishAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishAuctionApplication.class, args);
+	}
+
+}
---
+++ fishAuction/src/test/java/com/example/fishAuction/FishAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fishAuction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
