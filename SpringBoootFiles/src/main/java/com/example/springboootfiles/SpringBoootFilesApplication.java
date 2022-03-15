4aa10e5426ad102b61b79a6789ba44d9fde932f6
canrollas <canrollas@gmail.com>
2022-03-16 01:22:44 +0300

    Spring Boot Appended -2

---
+++ SpringBoootFiles/src/main/java/com/example/springboootfiles/SpringBoootFilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboootfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoootFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoootFilesApplication.class, args);
+    }
+
+}
---
+++ SpringBoootFiles/src/test/java/com/example/springboootfiles/SpringBoootFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboootfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoootFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
